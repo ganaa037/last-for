552249d333a8e3e39d53baf97afa0f12e72db90b
ganaa037 <tuvaangantumur@gmail.com>
2025-01-24 12:59:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+// let text = "Ad est cupidatat culpa dolor";
+// let words = text.split(" ");
+// console.log(words);
+
+// let text = "  Et dolor culpa do deserunt exercitation proident enim   ";
+// let text2 = text.trim().split(" ");
+// console.log(text2);
+
+// const text = "Eu commodo consequat do cillum eu occaecat deserunt enim";
+// const array = text.split(" ");
+// for (i = 0; i < array.length; i++) {
+//   if (array[i].includes("a")) {
+//     console.log(array[i]);
+//   }
+// }
+
+// for (i = 0; i < text.length; i++) {
+//   if (text[i] == "l") {
+//     count = count + 1;
+//   }
+// }
+
+// char loop
+// 1-3 size
+// size === 3 array.push
+
+// exercise4
+
+// const fruits = ["apple", "banana", "cherry", "Date"];
+// const upperCasefruits = fruits.map((value) => {
+//   return value.toUpperCase();
+// });
+// console.log(upperCasefruits);
+
+// const fruits = ["apple", "banana", "cherry", "Date"];
+// const result = fruits.filter((value, index) => {
+//   return value.length > 5;
+// });
+// console.log(result);
+
+// const arr = [4, -2, 0, 3, 2];
+// // let temp = arr[0];
+// // arr[0] = arr[1];
+// // arr[1] = temp;
+// // console.log(arr);
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i; j < arr.length; j++) {
+//     if (arr[i + 1] != undefined) {
+//       if (arr[i] > arr[j + 1]) {
+//         let temp = arr[i];
+//         arr[i] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+// }
+// console.log(arr);
+
+function str(num1, num2) {
+  for (let i = 0; i < num1.length; i++ )
+    
+}
